b3037d11de6d22828fd8ff0ccfa07fa30e53a670
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-02-20 19:24:07 -0300

    Changes to make the code more readable.

--- src/main.js
+++ src/main.js
@@ -17,49 +17,79 @@ const fetchPokemonData = (pokemon) => {
 };
 
 const renderData = (pokeData) => {
-  const gifSprite = pokeData.sprites.versions["generation-v"]["black-white"].animated;
-
   const container = document.querySelector("#container");
-  const divs = [...container.children];
-  divs.sort((a, b) => a.id - b.id);
-  divs.forEach((div) => container.appendChild(div));
 
   const pokemonContainer = document.createElement("div");
   pokemonContainer.id = `${pokeData.id}`;
-  pokemonContainer.className = "bg-white p-6 rounded-lg shadow-xl text-center cursor-pointer hover:-translate-y-1 transition-all duration-200";
-
-  const name = document.createElement("h2");
-  name.innerHTML = pokeData.name;
-  name.className = "mb-2 font-bold text-2xl text-gray-600";
+  pokemonContainer.classList = `
+  relative
+  bg-white 
+  p-6 
+  rounded-lg 
+  shadow-xl 
+  text-center 
+  cursor-pointer 
+  hover:-translate-y-1 transition-all duration-200
+  `;
 
   const id = document.createElement("p");
-  id.innerHTML = `N° ${pokeData.id}`;
+  id.innerHTML = `#${pokeData.id}`;
+  id.classList = `
+  absolute
+  text-sky-200 
+  text-2xl 
+  font-bold
+  `;
 
-  const type = document.createElement("ul");
-  type.classList = "";
+  const gifSprite =
+    pokeData.sprites.versions["generation-v"]["black-white"].animated;
 
   const sprite = document.createElement("div");
-  sprite.classList = "flex flex-col justify-center items-center";
+  sprite.classList = `
+  flex flex-col 
+  justify-center 
+  items-center
+  `;
 
+  const name = document.createElement("h2");
+  name.innerHTML = `${pokeData.name}`.toUpperCase();
+  name.classList = `
+  font-bold 
+  text-xl 
+  text-sky-900 
+  mt-5
+  `;
+
+  const type = document.createElement("div");
+  type.classList = "mt-5";
+
+  sortCardsById(container);
   createTypes(pokeData.types, type);
   createPokeImage(gifSprite, sprite);
 
-  pokemonContainer.append(sprite, id, name, type);
+  pokemonContainer.append(id, sprite, name, type);
   container.appendChild(pokemonContainer);
 };
 
-const createTypes = (types, ul) => {
+const sortCardsById = (container) => {
+  const divs = [...container.children];
+  divs.sort((a, b) => a.id - b.id);
+  divs.forEach((div) => container.appendChild(div));
+};
+
+const createTypes = (types, div) => {
   types.forEach((type) => {
-    const typeP = document.createElement("li");
-    typeP.classList = "text-gray-500";
+    const typeP = document.createElement("span");
+    typeP.classList = "text-red-700";
     typeP.innerHTML = type["type"]["name"];
-    ul.appendChild(typeP);
+    div.appendChild(typeP);
   });
 };
 
 const createPokeImage = (sprites, div) => {
   const pokeImage = document.createElement("img");
   pokeImage.srcset = sprites.front_default;
+  pokeImage.classList = "mt-5";
   div.appendChild(pokeImage);
 };
 
