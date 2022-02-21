0fd79e3dd9615e6758c6d341ca31f982aac5b389
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-02-20 22:44:46 -0300

    Adds styles depending on the type..

--- src/main.js
+++ src/main.js
@@ -35,14 +35,13 @@ const renderData = (pokeData) => {
   const id = document.createElement("p");
   id.innerHTML = `#${pokeData.id}`;
   id.classList = `
-  absolute
-  text-sky-200 
+  text-gray-600 
   text-2xl 
-  font-bold
+  font-extrabold
   `;
 
   const gifSprite =
-    pokeData.sprites.versions["generation-v"]["black-white"].animated;
+    pokeData.sprites.versions["generation-v"]["black-white"];
 
   const sprite = document.createElement("div");
   sprite.classList = `
@@ -54,14 +53,17 @@ const renderData = (pokeData) => {
   const name = document.createElement("h2");
   name.innerHTML = `${pokeData.name}`.toUpperCase();
   name.classList = `
-  font-bold 
+  font-extrabold 
   text-xl 
-  text-sky-900 
+  text-gray-900 
   mt-5
   `;
 
   const type = document.createElement("div");
-  type.classList = "mt-5";
+  type.classList = `
+  mt-5
+  font-bold
+  `;
 
   sortCardsById(container);
   createTypes(pokeData.types, type);
@@ -79,10 +81,12 @@ const sortCardsById = (container) => {
 
 const createTypes = (types, div) => {
   types.forEach((type) => {
-    const typeP = document.createElement("span");
-    typeP.classList = "text-red-700";
-    typeP.innerHTML = type["type"]["name"];
-    div.appendChild(typeP);
+    const typeSpan = document.createElement("span");
+    typeSpan.innerHTML = type["type"]["name"].toUpperCase();
+
+    createTypeColor(typeSpan, type);
+
+    div.appendChild(typeSpan);
   });
 };
 
@@ -91,6 +95,46 @@ const createPokeImage = (sprites, div) => {
   pokeImage.srcset = sprites.front_default;
   pokeImage.classList = "mt-5";
   div.appendChild(pokeImage);
+};
+
+const createTypeColor = (element, type) => {
+  if (type["type"]["name"] === "grass") {
+    element.classList = "bg-green-500 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "poison") {
+    element.classList = "bg-violet-500 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "fire") {
+    element.classList = "bg-orange-500 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "bug") {
+    element.classList = "bg-lime-300 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "flying") {
+    element.classList = "bg-indigo-300 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "water") {
+    element.classList = "bg-blue-400 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "normal") {
+    element.classList = "bg-neutral-400 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "psychic") {
+    element.classList = "bg-red-500 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "dark") {
+    element.classList = "bg-zinc-800 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "dragon") {
+    element.classList = "bg-blue-600 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "electric") {
+    element.classList = "bg-yellow-400 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "fairy") {
+    element.classList = "bg-pink-500 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "fighting") {
+    element.classList = "bg-rose-600 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "ghost") {
+    element.classList = "bg-indigo-600 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "ground") {
+    element.classList = "bg-amber-700 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "ice") {
+    element.classList = "bg-teal-400 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "rock") {
+    element.classList = "bg-amber-900 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  } else if (type["type"]["name"] === "steel") {
+    element.classList = "bg-sky-700 text-gray-800 text-bold rounded py-1 px-1.5 mr-1";
+  }
 };
 
 fetchPokemon();
