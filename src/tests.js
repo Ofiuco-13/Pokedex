de36c8322c753e297723aac19d83f205f4bd829f
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-02 21:01:53 -0300

    Adds unit tests

---
+++ src/tests.js
@@ -0,0 +1,24 @@
+const testFetchPokemon = (res) => {
+  console.assert(
+    res.results.length === 12,
+    "Error: The amount of pokemons was not 12!"
+  );
+};
+
+const testShowSidebar = (element) => {
+  console.assert(
+    element.innerHTML !== "\n        ",
+    "Error: The sidebar showed with no content!"
+  );
+};
+
+const testRemovePokeData = (element) => {
+  console.assert(
+    element.innerHTML === "\n        ",
+    "Error: Sidebar cannot be displayed!"
+  );
+};
+
+const testRenderData = (res) => {
+  console.assert(res.id > 0, "Error: renderData worked when id was below 1");
+};
